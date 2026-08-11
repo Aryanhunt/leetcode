27b50b6b08e5157beb8e93a4e6c03c1c4235e24f
Aryan Chaubey <aryavrath9@gmail.com>
2026-08-11 20:31:23 +0530

    Time: 453 ms (76.69%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3803-find-products-with-valid-serial-numbers/find-products-with-valid-serial-numbers.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT 
+    product_id, 
+    product_name, 
+    description
+FROM 
+    Products
+WHERE 
+    REGEXP_LIKE(description, '\\bSN[0-9]{4}-[0-9]{4}\\b', 'c')
+ORDER BY 
+    product_id ASC;
