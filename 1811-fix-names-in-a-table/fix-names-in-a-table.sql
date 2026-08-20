ebbbab935d88046e2ab8030974fe41c0b78b59a6
Aryan Chaubey <aryavrath9@gmail.com>
2026-08-20 22:55:31 +0530

    Time: 681 ms (65.76%) | Memory: 0B (100.00%) - LeetSync

--- 1811-fix-names-in-a-table/fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -1,5 +1,9 @@
+# Write your MySQL query statement below
 SELECT
     user_id,
-    UPPER(SUBSTR(name, 1, 1)) || LOWER(SUBSTR(name, 2)) AS name
+    CONCAT(
+        UPPER(LEFT(name, 1)),
+        LOWER(SUBSTRING(name, 2))
+    ) AS name
 FROM Users
 ORDER BY user_id;
